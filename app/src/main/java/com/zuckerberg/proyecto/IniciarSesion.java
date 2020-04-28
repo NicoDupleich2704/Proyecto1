3c691dbc51c08711d6f615271a5792236524619a
NicoDupleich2704 <62887586+NicoDupleich2704@users.noreply.github.com>
2020-04-28 19:22:23 -0400

    avance botones

--- app/src/main/java/com/zuckerberg/proyecto/IniciarSesion.java
+++ app/src/main/java/com/zuckerberg/proyecto/IniciarSesion.java
@@ -2,13 +2,26 @@ package com.zuckerberg.proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class IniciarSesion extends AppCompatActivity {
+
+    Button inresarCompras;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_sesion);
+
+        inresarCompras = (Button)findViewById(R.id.BotonIngresar);
+
+    }
+
+    public void IngresarComprar (View view){
+        Intent comprar = new Intent(this, MainActivity.class);
+        startActivity(comprar);
     }
 }
