42ae9f0e170b326c094ccf067c7dc1b10dee8a93
NicoDupleich2704 <62887586+NicoDupleich2704@users.noreply.github.com>
2020-05-06 19:06:13 -0400

    image buttons

--- app/src/main/java/com/zuckerberg/proyecto/MainActivity.java
+++ app/src/main/java/com/zuckerberg/proyecto/MainActivity.java
@@ -6,11 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button ingresarDoctores;
-    Button ingresarTecnicos;
+    ImageButton ingresarDoctores;
+    ImageButton ingresarTecnicos;
 
 
     @Override
@@ -18,8 +19,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ingresarDoctores = (Button)findViewById(R.id.BotonMedicos);
-        ingresarTecnicos = (Button)findViewById(R.id.BotonTecnicos);
+        ingresarDoctores = (ImageButton)findViewById(R.id.BotonMedicos);
+        ingresarTecnicos = (ImageButton)findViewById(R.id.BotonTecnicos);
 
 
     }
