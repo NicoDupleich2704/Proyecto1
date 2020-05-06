5b0e4370449bd69fbf2e49ee596317df963d23fb
Nicolas Abdo <nicoabdoj@gmail.com>
2020-05-06 17:07:35 -0400

    Merge branch 'master' of https://github.com/NicoDupleich2704/Proyecto1
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

---
+++ app/src/main/java/com/zuckerberg/proyecto/AnimalesActivity.java
@@ -0,0 +1,14 @@
+package com.zuckerberg.proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnimalesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animales);
+    }
+}
