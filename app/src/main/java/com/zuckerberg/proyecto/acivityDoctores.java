74083d54514d1f783c46edcd1a1787927c7453be
Jhoel17 <62887551+Jhoel17@users.noreply.github.com>
2020-05-12 19:58:30 -0400

    Solucion error

--- app/src/main/java/com/zuckerberg/proyecto/acivityDoctores.java
+++ app/src/main/java/com/zuckerberg/proyecto/acivityDoctores.java
@@ -8,22 +8,13 @@ import android.widget.Button;
 
 public class acivityDoctores extends AppCompatActivity {
 
-    Button botonEnviar;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acivity_doctores);
 
-        botonEnviar.findViewById(R.id.boton_enviar);
-
-
-        botonEnviar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
 
     }
 }
