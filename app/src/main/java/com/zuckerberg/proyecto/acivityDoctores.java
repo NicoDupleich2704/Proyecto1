2a610aa091597ec36477e9ea1ea1e21c3c935947
Jhoel17 <62887551+Jhoel17@users.noreply.github.com>
2020-05-12 18:05:11 -0400

    Medicos layout

--- app/src/main/java/com/zuckerberg/proyecto/acivityDoctores.java
+++ app/src/main/java/com/zuckerberg/proyecto/acivityDoctores.java
@@ -3,12 +3,27 @@ package com.zuckerberg.proyecto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class acivityDoctores extends AppCompatActivity {
+
+    Button botonEnviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acivity_doctores);
+
+        botonEnviar.findViewById(R.id.boton_enviar);
+
+
+        botonEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
