2a208c898bbfd7b24e5113ab0a9b21e1292ee37a
Jhoel17 <62887551+Jhoel17@users.noreply.github.com>
2020-05-13 09:06:55 -0400

    botones interaccion

--- app/src/main/java/com/zuckerberg/proyecto/TecnicosActivity.java
+++ app/src/main/java/com/zuckerberg/proyecto/TecnicosActivity.java
@@ -3,10 +3,17 @@ package com.zuckerberg.proyecto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class TecnicosActivity extends AppCompatActivity {
+
+    Button botonSiguiente;
+    EditText problema;
+    EditText tipoDeTecnico;
+    
 
 
     @Override
@@ -14,6 +21,30 @@ public class TecnicosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tecnicos);
 
+        botonSiguiente = findViewById(R.id.boton_para_enviar);
+        tipoDeTecnico = findViewById(R.id.tecnico_requerido);
+        problema = findViewById(R.id.problem);
+
+
+
+
+
+        botonSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (problema.getEditableText()!=null && tipoDeTecnico.getEditableText()!= null){
+                    botonSiguiente.setEnabled(true);
+                    if(botonSiguiente.isEnabled()){
+                        finish();
+                    }
+                    else{
+                        Toast.makeText(TecnicosActivity.this, R.string.mensaje_no_enviar, Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+
+            }
+        });
 
 
     }
