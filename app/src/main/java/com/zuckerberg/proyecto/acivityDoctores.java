63636291108996e688f6127abb997be8d2ea91e3
Jhoel17 <62887551+Jhoel17@users.noreply.github.com>
2020-05-13 19:55:02 -0400

    doctores spiner

--- app/src/main/java/com/zuckerberg/proyecto/acivityDoctores.java
+++ app/src/main/java/com/zuckerberg/proyecto/acivityDoctores.java
@@ -4,10 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class acivityDoctores extends AppCompatActivity {
 
+    ArrayList<String> ListaEsp = new ArrayList<>();
+
+    Spinner lista;
 
 
     @Override
@@ -15,6 +22,23 @@ public class acivityDoctores extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acivity_doctores);
 
+        lista = findViewById(R.id.listaEspecialistas);
 
+        ListaEsp.add("Médico General");
+        ListaEsp.add("Dentista");
+        ListaEsp.add("Dermatólogo");
+        ListaEsp.add("Endocrinólogo");
+        ListaEsp.add("Neurólogo");
+        ListaEsp.add("Oftalmólogo");
+        ListaEsp.add("Otorrinolaringólogo");
+        ListaEsp.add("Pediatra");
+        ListaEsp.add("Psiquiatra");
+        ListaEsp.add("Terapeuta");
+        ListaEsp.add("Traumatólogo");
+
+
+
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, ListaEsp);
+        lista.setAdapter(adaptador);
     }
 }
