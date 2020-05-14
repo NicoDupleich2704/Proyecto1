7596a3d136df5da6eee394acb555cdcd197bda79
NicoDupleich2704 <62887586+NicoDupleich2704@users.noreply.github.com>
2020-05-14 03:03:58 -0400

    add

--- app/src/main/java/com/zuckerberg/proyecto/ProductoSharedPreferencesManager.java
+++ app/src/main/java/com/zuckerberg/proyecto/ProductoSharedPreferencesManager.java
@@ -7,6 +7,10 @@ import android.widget.Toast;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.internal.$Gson$Preconditions;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class ProductoSharedPreferencesManager {
 
@@ -16,16 +20,41 @@ public class ProductoSharedPreferencesManager {
             this.context = context;
     }
 
-    public void addToCart(Productos productos){
+    public static void addToCart(Productos productos){
+
+        ArrayList<Productos> currentProducts = getProducts();
+        currentProducts.add(productos);
+
         SharedPreferences sharedPreferences = context.getSharedPreferences("ShoppingCart", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
 
         Gson gson = new Gson();
-        String productString = gson.toJson(productos);
+        String productString = gson.toJson(currentProducts);
 
         editor.putString("productos", productString);
         editor.apply();
 
         Toast.makeText(context, "Producto añadido al carrito", Toast.LENGTH_SHORT).show();
     }
+
+    public ArrayList<Productos> getProducts(){
+        SharedPreferences sharedPreferences = context.getSharedPreferences("ShoppingCart", Context.MODE_PRIVATE);
+        String productosString = sharedPreferences.getString("productos", "[]");
+
+        Gson gson = new Gson();
+        Type listType = new TypeToken<ArrayList<Productos>>(){}.getType();
+        ArrayList<Productos> productos = gson.fromJson(productosString, listType);
+
+        return productos;
+    }
+
+    public int getTotal(){
+        int total = 0;
+        for(Productos producto : getProducts()){
+            total += producto.precio;
+        }
+        return total;
+    }
+
 }
+
