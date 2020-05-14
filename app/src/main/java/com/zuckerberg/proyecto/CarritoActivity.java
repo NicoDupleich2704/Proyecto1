3ca397bbd69fe3cff5de3f856817f453eb15618f
Jhoel17 <62887551+Jhoel17@users.noreply.github.com>
2020-05-14 06:29:19 -0400

    finalizar compra

--- app/src/main/java/com/zuckerberg/proyecto/CarritoActivity.java
+++ app/src/main/java/com/zuckerberg/proyecto/CarritoActivity.java
@@ -1,10 +1,15 @@
 package com.zuckerberg.proyecto;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -13,6 +18,8 @@ public class CarritoActivity extends AppCompatActivity {
     RecyclerView rvShoppingCart;
     ProductoSharedPreferencesManager productoSharedPreferencesManager;
 
+    Button finalizar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,7 @@ public class CarritoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_carrito);
 
         rvShoppingCart = findViewById(R.id.rvShoppingCart);
+        finalizar = findViewById(R.id.finalizar);
 
         productoSharedPreferencesManager = new ProductoSharedPreferencesManager(this);
 
@@ -37,6 +45,34 @@ public class CarritoActivity extends AppCompatActivity {
         rvShoppingCart.setAdapter(adapter);
         rvShoppingCart.setLayoutManager(new LinearLayoutManager(this));
 
+        finalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarMensaje();
+            }
+        });
 
+
+    }
+
+    private void mostrarMensaje(){
+        AlertDialog.Builder mensaje = new AlertDialog.Builder(this);
+        mensaje.setTitle("Finalizar");
+        mensaje.setMessage("Desea finalizar la compra?");
+        mensaje.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+                Toast.makeText(CarritoActivity.this, "Compra exitosa", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mensaje.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                
+                Toast.makeText(CarritoActivity.this, "Cancelado", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mensaje.show();
     }
 }
